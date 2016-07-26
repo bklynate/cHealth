78b37a3fac26e47c3e1d7c55dd29ace73938fed4
twiga <mail@valentinemwangi.com>
2016-07-26 23:52:27 +0300

    Fix inventory font awesome icon

--- resources/views/templates/pharmacy/blocks.blade.php
+++ resources/views/templates/pharmacy/blocks.blade.php
@@ -24,11 +24,10 @@
                 <span class="text-white font-thin h1 block"><i class="fa fa-stethoscope"></i></span>
                 <span class="text-muted text-xs">Inventory</span>
                 <span class="bottom text-right w-full">
-                  <i class="fa fa-stethoscope text-muted m-r-sm"></i>
+                  <i class="fa fa-database text-muted m-r-sm"></i>
                 </span>
               </a>
             </div>
-
             <div class="col-xs-6">
               <a href="" class="block panel padder-v bg-info item dker">
                 <span class="text-white font-thin h1 block">5</span>
