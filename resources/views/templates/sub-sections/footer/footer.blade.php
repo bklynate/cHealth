7c38915ecaa4e57f9249255dbc90c96b6065d62a
twiga <mail@valentinemwangi.com>
2016-07-31 23:39:24 +0300

    Remove footer title to generalize

--- resources/views/templates/sub-sections/footer/footer.blade.php
+++ resources/views/templates/sub-sections/footer/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="app-footer" role="footer">
     <div class="wrapper b-t bg-light">
-      <small>&copy; <?php echo date("Y"); ?> Cimplicity Apps. All Rights Reserved.</small>
+      <small>&copy; <?php echo date("Y"); ?> All Rights Reserved.</small>
     </div>
   </footer>
